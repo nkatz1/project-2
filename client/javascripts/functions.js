bfe5f268149edf696a262d2c8ec6f07294b4799d
nkatz1 <nkatz@uoregon.edu>
2020-04-24 10:35:21 -0700

    Added Lab part 1

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,32 @@
 // jshint esversion: 6
+//Req. A
+let revWords1 = str => {
+  let result = "";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
 
+  return result;
+};
+console.log(revWords1("Thurs"));
+
+//
+
+//Req. B
+
+//Req. C
+let revWords3 = str => {
+  let arr = str.split("");
+  let result = "";
+  for (char of arr) {
+    result = char + result;
+  }
+  return result;
+};
+
+console.log(revWords3("Thurs"));
+
+//XC
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,19 +1,20 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function() {
+  let prefixURL =
+    "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
+  let suffixURL = "&format=json&jsoncallback=?";
+  //get value entered by user from textbox
+  let flickrTag = $("input").val();
+  console.log(flickrTag);
+  let requestURL = prefixURL + flickrTag + suffixURL;
+  console.log(requestURL);
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
-    let suffixURL = "&format=json&jsoncallback=?";
-    //get value entered by user from textbox
-    let flickrTag = $("input").???();
-    let requestURL = prefixURL + flickrTag + suffixURL;
-
-    //clear old photos
-    $(".photos").???("");
+  //clear old photos
+  $(".photos").html("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
-
       //Flickr returns 20 images by default
       //We need only six images for the Gallery
       if (index < 6) {
@@ -24,7 +25,7 @@ let main = function(){
         // set the attribute to the url
         // contained in the response
         $img.attr("src", item.media.m);
-        $img.attr("width", "100");
+        $img.attr("width", "150");
 
         // attach the img tag to the main
         // photos element and then fade it in
@@ -32,9 +33,16 @@ let main = function(){
         $img.fadeIn();
       }
     });
-
   });
 };
 
+//$(document).ready(controller);
 
-$(document).ready(main);
+//Register the controller after the DOM is complete
+window.addEventListener("load", () => {
+  //select the button
+  let button = document.querySelector("button");
+
+  //register the click handler for the button
+  button.addEventListener("click", controller);
+});
